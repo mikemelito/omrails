0ca5aecbb435a40490fc74507c25a44ca1422681
Michael Melito <michael.j.melito@gmail.com>
2013-09-24 00:41:46 -0500

    Added Home and About pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Omrails::Application.routes.draw do
+  get 'about' => 'pages#about'
+  root :to => 'Pages#home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
